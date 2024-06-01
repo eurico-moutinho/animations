9705778967223665649490121310830369ac4a55
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2024-06-02 00:23:06 +0100

    Update Gallery.vue

--- src/views/Gallery/Gallery.vue
+++ src/views/Gallery/Gallery.vue
@@ -23,11 +23,11 @@ export default {
                     const clientX = 'clientX' in event ? event.clientX : event.touches[0].clientX;
                     
                     if(window.innerWidth < 768) {
-                        this.x -= (this.lastPositionX - clientX) / 8;
+                        this.x -= (this.lastPositionX - clientX) / 1;
                         box.style.transform = "perspective(1000px) scale(0.7) rotateY(" + this.x + "deg)";
 
                     } else {
-                        this.x -= (this.lastPositionX - clientX) / 0.5;
+                        this.x -= (this.lastPositionX - clientX) / 30;
                         box.style.transform = "perspective(1000px) rotateY(" + this.x + "deg)";
 
                     }
