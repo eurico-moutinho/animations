d8bb808dfdfe9fe087ada9c0c211c5ec8ff49aa5
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2024-06-02 00:15:53 +0100

    Update LightShadow.vue

--- src/views/LightShadow/LightShadow.vue
+++ src/views/LightShadow/LightShadow.vue
@@ -13,15 +13,18 @@ export default {
     this.shadowText = document.getElementById('light-shadow-text');
     this.shadowLight = document.getElementById('light-shadow-light');
 
-    document.addEventListener('mousemove', (event: MouseEvent): void => {
+    document.addEventListener('mousemove', (event: MouseEvent | TouchEvent): void => {
         
         if (this.shadowLight && this.shadowText) {
 
-            this.shadowLight.style.left = event.clientX + 'px';
-            this.shadowLight.style.top = event.clientY + 'px';
+            const clientX = 'clientX' in event ? event.clientX : event.touches[0].clientX;
+            const clientY = 'clientY' in event ? event.clientY : event.touches[0].clientY;
 
-            const distanceX = event.clientX - this.shadowText.offsetLeft - this.shadowLight.offsetWidth / 2;
-            const distanceY = event.clientY - this.shadowText.offsetTop - this.shadowLight.offsetHeight / 2;
+            this.shadowLight.style.left = clientX + 'px';
+            this.shadowLight.style.top =  clientY + 'px';
+
+            const distanceX = clientX - this.shadowText.offsetLeft - this.shadowLight.offsetWidth / 2;
+            const distanceY = clientY - this.shadowText.offsetTop - this.shadowLight.offsetHeight / 2;
 
             let newShadow: string = '';
             let shadowX: number = 0;
